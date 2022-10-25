1dd1531d3b2b07cb61674dae8b0288b7d827087d
Victor Rodrigues <Victordarkiin@gmail.com>
2022-10-25 12:06:20 -0300

    Organizando código js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,4 @@
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
 
 function convertPokemonToLi(pokemon) {
     return ` 
@@ -23,10 +21,7 @@ function convertPokemonToLi(pokemon) {
 
 const pokemonList = document.getElementById('pokemonList');
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => {
+pokeAPI.getPokemons().then((pokemons) => {
         for (let i = 0; i < pokemons.length; i++) {
             const pokemon = pokemons[i];
             pokemonList.innerHTML += convertPokemonToLi(pokemon);
