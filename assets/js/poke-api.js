46339952bfa2365db1d0a92494fe2a90fc306a2a
Victor Rodrigues <Victordarkiin@gmail.com>
2022-10-25 13:06:57 -0300

    Realizando requisições de pokemons

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,9 +1,16 @@
 const pokeAPI = {};
 
-pokeAPI.getPokemons = (offset = 0, limit = 10) => {
+pokeAPI.getPokemonDetails = (pokemon) => {
+    return fetch(pokemon.url).then((response) => response.json())
+}
+
+pokeAPI.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
     return fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .catch((error) => console.error(error))
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(pokeAPI.getPokemonDetails))
+        .then((detailRequests => Promise.all(detailRequests)))
+        .then((pokemonDetails) => pokemonDetails)
+        .catch((error) => console.error(error))
 }
